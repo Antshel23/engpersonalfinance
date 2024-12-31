f8549cfc79415a8906a9c7cc97c57a84e9d64ad5
Antshel23 <antsheldon23@icloud.com>
2024-12-31 16:29:05 +0000

    completed basic pension calcs

--- backend/controllers/pension_controller.py
+++ backend/controllers/pension_controller.py
@@ -1,17 +1,33 @@
 from utils.pension_utils import calculate_pension_pot
-from flask import request
+from flask import request, jsonify
+import json
 
-def get_pension_pot():
+def get_user_pension_input():
     data = request.json
     current_salary = data.get('current_salary')
-    personal_contribution = data.get('personal_contribution',0)
-    employer_contribution = data.get('employer_contribution',0)
-    current_pot = data.get('current_pot',0)
+    personal_contribution = data.get('personal_contribution', 0)
+    employer_contribution = data.get('employer_contribution', 0)
+    current_pot = data.get('current_pot', 0)
     retirement_age = data.get('retirement_age')
     current_age = data.get('current_age')
     
-    if not current_salary or not retirement_age or current_age >= retirement_age or current_age < 16:
+    if not current_salary or not retirement_age or current_age >= retirement_age or current_age < 16 :
         return {"error": "Missing/incorrect required fields"}, 400
+    else:
+        return {
+            "current_salary": current_salary,
+            "personal_contribution": personal_contribution,
+            "employer_contribution": employer_contribution,
+            "current_pot": current_pot,
+            "retirement_age": retirement_age,
+            "current_age": current_age,
+        }
+
+def return_pension_pot():
+    user_input = get_user_pension_input()
     
-    pension_pot = calculate_pension_pot(current_salary, personal_contribution, employer_contribution, current_pot, retirement_age, current_age)
-    return {'Pension pot': pension_pot}
+    if isinstance(user_input, dict) and "error" in user_input:
+        return jsonify(user_input), 400
+    
+    pension_pot_data = json.loads(calculate_pension_pot(**user_input))
+    return pension_pot_data, 200
--- backend/utils/pension_utils.py
+++ backend/utils/pension_utils.py
@@ -1,12 +1,76 @@
+import json
+
 def calculate_pension_pot(current_salary, personal_contribution, employer_contribution, current_pot, retirement_age, current_age):
-    personal_contribution = current_salary * personal_contribution / 100
-    employer_contribution = current_salary * employer_contribution / 100
-    total_contribution = personal_contribution + employer_contribution
     years_to_retirement = retirement_age - current_age
-    good_portfolio_return = 1.0425
+    salary_growth_rate = 1.02
+    annual_portfolio_charge = 0.0075
+    inflation_rate = 0.02
     
-    for year in range(1, years_to_retirement + 1):
-        current_pot += total_contribution
-        current_pot *= good_portfolio_return
+    good_portfolio_return = ((1.055 * (1 - annual_portfolio_charge)) / (1 + inflation_rate))
+    med_portfolio_return = ((1.035 * (1 - annual_portfolio_charge)) / (1 + inflation_rate))
+    inflation_match_portfolio_return = ((1.02 * (1 - annual_portfolio_charge)) / (1 + inflation_rate))
+    inflation_loss_portfolio_return = ((1.0 * (1 - annual_portfolio_charge)) / (1 + inflation_rate))
     
-    return round(current_pot, 2)
+    def calculate_pot(return_rate):
+        pot = current_pot
+        salary = current_salary
+        for year in range(1, years_to_retirement + 1):
+            salary *= salary_growth_rate
+            annual_contribution = ((personal_contribution + employer_contribution) * salary) / 100
+            pot += annual_contribution
+            pot *= return_rate
+        return round(pot, 0)
+    
+    def calculate_drawdown(pot):
+        annual_drawdown = pot * 0.04
+        return round(annual_drawdown, 0)
+    
+    def calculate_lumpsum_drawdown(pot):
+        lumpsum = pot * 0.25
+        pot -= lumpsum
+        annual_drawdown = pot * 0.04
+        return {
+            "lumpsum": round(lumpsum),
+            "drawdown": round(annual_drawdown, 0)
+        }
+    result = {
+        "current_salary": current_salary,
+        "personal_contribution": personal_contribution,
+        "employer_contribution": employer_contribution,
+        "current_pot": current_pot,
+        "retirement_age": retirement_age,
+        "current_age": current_age,
+        "years_to_retirement": years_to_retirement,
+        "calculated_pots": {
+            "high": calculate_pot(good_portfolio_return),
+            "medium": calculate_pot(med_portfolio_return),
+            "inflation_match": calculate_pot(inflation_match_portfolio_return),
+            "inflation_loss": calculate_pot(inflation_loss_portfolio_return)
+        },
+        "drawdown": {
+            "high": calculate_drawdown(calculate_pot(good_portfolio_return)),
+            "medium": calculate_drawdown(calculate_pot(med_portfolio_return)),
+            "inflation_match": calculate_drawdown(calculate_pot(inflation_match_portfolio_return)),
+            "inflation_loss": calculate_drawdown(calculate_pot(inflation_loss_portfolio_return))
+        }, 
+        "lumpsum_drawdown": {
+            "high": {
+                "lumpsum": calculate_lumpsum_drawdown(calculate_pot(good_portfolio_return))["lumpsum"],
+                "drawdown": calculate_lumpsum_drawdown(calculate_pot(good_portfolio_return))["drawdown"]
+            },
+            "medium": {
+                "lumpsum": calculate_lumpsum_drawdown(calculate_pot(med_portfolio_return))["lumpsum"],
+                "drawdown": calculate_lumpsum_drawdown(calculate_pot(med_portfolio_return))["drawdown"]
+            },
+            "inflation_match": {
+                "lumpsum": calculate_lumpsum_drawdown(calculate_pot(inflation_match_portfolio_return))["lumpsum"],
+                "drawdown": calculate_lumpsum_drawdown(calculate_pot(inflation_match_portfolio_return))["drawdown"]
+            },
+            "inflation_loss": {
+                "lumpsum": calculate_lumpsum_drawdown(calculate_pot(inflation_loss_portfolio_return))["lumpsum"],
+                "drawdown": calculate_lumpsum_drawdown(calculate_pot(inflation_loss_portfolio_return))["drawdown"]
+            }
+            }
+        }
+    
+    return json.dumps(result, indent=4)
